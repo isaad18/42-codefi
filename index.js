0818c1098f5754e7bb176cea22660ae0f76a6d09
saim <saim.test123@gmail.com>
2022-10-29 04:36:53 +0400

    login page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+app.use(express.static(__dirname + '/assets'));
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname + "/"});
 });
